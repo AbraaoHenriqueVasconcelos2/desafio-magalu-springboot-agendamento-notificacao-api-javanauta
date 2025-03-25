46626496aec2b44793b906fcfef4944a4275543e
AbraaoHenriqueVasconcelos2 <abraao.vscncls@gmail.com>
2025-03-24 21:54:19 -0300

    first commit

---
+++ src/main/java/com/exemplo/demo/desafio_magalu_agendamento_notificacao_api/DesafioMagaluAgendamentoNotificacaoApiApplication.java
@@ -0,0 +1,13 @@
+package com.exemplo.demo.desafio_magalu_agendamento_notificacao_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioMagaluAgendamentoNotificacaoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioMagaluAgendamentoNotificacaoApiApplication.class, args);
+	}
+
+}
